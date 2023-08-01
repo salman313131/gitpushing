e76d816fce96fcc54e1fd422a38a503a15fffd7c
salman <salmankhan.faze@gmail.com>
2023-08-01 07:14:28 +0530

    edit button added

--- script.js
+++ script.js
@@ -20,9 +20,13 @@ function onSubmit(e){
         const newLi = document.createElement('li')
         newLi.appendChild(document.createTextNode(`${userName.value} ${email.value}`));
         const newBtn = document.createElement('button')
-        newBtn.textContent = ' Delete'
-        newBtn.className = 'delete'
+        const newEdit = document.createElement('button')
+        newEdit.textContent = 'Edit';
+        newBtn.textContent = ' Delete';
+        newBtn.className = 'delete';
+        newEdit.className = 'editUser';
         newLi.appendChild(newBtn);
+        newLi.appendChild(newEdit);
         users.appendChild(newLi);
         userName.value = '';
         email.value ='';
@@ -32,6 +36,17 @@ function removeItem(e){
     if (e.target.classList.contains('delete')){
         const li = e.target.parentElement;
         const listLi = li.textContent.split(" ");
+        items.removeChild(li)
+        localStorage.removeItem(listLi[1])
+    }
+    if (e.target.classList.contains('editUser')){
+        e.preventDefault();
+        const li = e.target.parentElement;
+        const listLi = li.textContent.split(" ");
+        const userName = document.querySelector('#name')
+        const email = document.querySelector('#email')
+        userName.value = listLi[0]
+        email.value = listLi[1]
         items.removeChild(li)
         localStorage.removeItem(listLi[1])
     }
